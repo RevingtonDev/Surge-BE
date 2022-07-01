9e6a7a9410de76aa6cf4b3793d2c64fd8d930518
RevingtonDev <revington.dev@gmail.com>
2022-07-01 18:49:08 +0530

    Initial Commit.

---
+++ app/src/main/java/dev/revington/Application.java
@@ -0,0 +1,15 @@
+package dev.revington;
+
+import net.minidev.json.JSONObject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
