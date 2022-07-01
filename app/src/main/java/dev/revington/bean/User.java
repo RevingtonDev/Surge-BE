a1fdfc5854ec61ba291e46131cb0f463a60fe15c
RevingtonDev <revington.dev@gmail.com>
2022-07-01 19:13:04 +0530

    User bean implementation.

---
+++ app/src/main/java/dev/revington/bean/User.java
@@ -0,0 +1,83 @@
+package dev.revington.bean;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+public class User {
+
+    @Id
+    private int Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date dateOfBirth;
+    private int mobile;
+    private boolean status;
+    private String accountType;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+}
