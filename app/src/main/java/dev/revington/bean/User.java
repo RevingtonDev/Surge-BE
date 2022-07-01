567f852a0b2e646144c0b41340c33a3f43396c3d
RevingtonDev <revington.dev@gmail.com>
2022-07-01 19:17:38 +0530

    variable name change from 'Id' to 'id'.

--- app/src/main/java/dev/revington/bean/User.java
+++ app/src/main/java/dev/revington/bean/User.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class User {
 
     @Id
-    private int Id;
+    private int id;
     private String firstName;
     private String lastName;
     private String email;
