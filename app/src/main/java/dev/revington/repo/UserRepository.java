43dbb8281306ee20e50ab08f78e8be4e12a972e7
RevingtonDev <revington.dev@gmail.com>
2022-07-02 02:37:07 +0530

    User repository implementation.

---
+++ app/src/main/java/dev/revington/repo/UserRepository.java
@@ -0,0 +1,36 @@
+package dev.revington.repo;
+
+import dev.revington.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    @Query(value =
+            "{$or : [" +
+                    "{" +
+                        "_id: {$regex: '?0'}" +
+                    "}," +
+                    "{" +
+                        "firstName: {$regex: '?0'}" +
+                    "}," +
+                    "{" +
+                        "lastName: {$regex: '?0'}" +
+                    "}," +
+                    "{" +
+                        "email: {$regex: '?0'}" +
+                    "}" +
+                    "]}")
+    public Page<User> find(String param, Pageable pageable);
+
+    public Page<User> findById(String param, Pageable pageable);
+    public Page<User> findByFirstName(String param, Pageable pageable);
+    public Page<User> findByLastName(String param, Pageable pageable);
+    public Page<User> findByEmail(String param, Pageable pageable);
+
+}
