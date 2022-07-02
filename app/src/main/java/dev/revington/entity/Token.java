6fe12d5123fa75768f0e7ce2ea46ed95fa2f15d6
RevingtonDev <revington.dev@gmail.com>
2022-07-02 22:48:10 +0530

    Entity 'expires' is a long value and an encryption key per token (beta - no usage yet).

--- app/src/main/java/dev/revington/entity/Token.java
+++ app/src/main/java/dev/revington/entity/Token.java
@@ -3,7 +3,9 @@ package dev.revington.entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.crypto.SecretKey;
 import java.sql.Timestamp;
+import java.util.Date;
 
 @Document(collection = "tokens")
 public class Token {
@@ -11,7 +13,16 @@ public class Token {
     @Id
     private int id;
     private String token;
-    private Timestamp expires;
+    private byte[] key;
+    private long expires;
+
+    public byte[] getKey() {
+        return key;
+    }
+
+    public void setKey(byte[] key) {
+        this.key = key;
+    }
 
     public int getId() {
         return id;
@@ -30,10 +41,11 @@ public class Token {
     }
 
     public Timestamp getExpires() {
-        return expires;
+        return new Timestamp(expires);
     }
 
     public void setExpires(Timestamp expires) {
-        this.expires = expires;
+        this.expires = expires.getTime();
     }
+
 }
