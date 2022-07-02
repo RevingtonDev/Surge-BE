0720c847ae0394858b39d686e4ff65e2247741a1
RevingtonDev <revington.dev@gmail.com>
2022-07-02 22:44:38 +0530

    API output won't contain null objects.

--- app/src/main/java/dev/revington/entity/User.java
+++ app/src/main/java/dev/revington/entity/User.java
@@ -1,7 +1,9 @@
 package dev.revington.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.IndexDirection;
@@ -11,6 +13,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 
 @Document(collection = "users")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
 
     @Id
