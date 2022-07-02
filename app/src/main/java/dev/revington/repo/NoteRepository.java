0b14089409a23c41dc9f5f4b388750235c54844b
RevingtonDev <revington.dev@gmail.com>
2022-07-02 22:40:54 +0530

    Interface changed.

--- app/src/main/java/dev/revington/repo/NoteRepository.java
+++ app/src/main/java/dev/revington/repo/NoteRepository.java
@@ -1,2 +1,14 @@
-package dev.revington.repo;public class NoteRepository {
+package dev.revington.repo;
+
+import dev.revington.entity.Note;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface NoteRepository extends MongoRepository<Note, Integer> {
+
+    @Query(value = "{'_id': '0'}")
+    public List<Note> findById(int id);
+
 }
