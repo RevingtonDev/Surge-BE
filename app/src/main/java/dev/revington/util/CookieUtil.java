57c688a457ea45c37ad00eccd24bdb6ce5156f84
RevingtonDev <revington.dev@gmail.com>
2022-07-02 22:59:43 +0530

    Cookie expiration set & get domain from http request - method implemented.

--- app/src/main/java/dev/revington/util/CookieUtil.java
+++ app/src/main/java/dev/revington/util/CookieUtil.java
@@ -6,19 +6,24 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 public class CookieUtil {
 
     public static void createCookie(HttpServletResponse res, String name, String value, String path, String domain, boolean http, boolean secure, long max) {
-        ResponseCookie.ResponseCookieBuilder cookieBuilder = ResponseCookie.from(name, value)
+        ResponseCookie.ResponseCookieBuilder cookieBuilder = ResponseCookie.from(name, Base64.getUrlEncoder().encodeToString(value.getBytes(StandardCharsets.UTF_8)))
                 .httpOnly(http)
-                .maxAge(max)
                 .secure(secure)
                 .path(path)
                 .domain(domain);
+        if (max > 0) {
+            cookieBuilder = cookieBuilder.maxAge(max);
+        }
         res.addHeader(HttpHeaders.SET_COOKIE, cookieBuilder.build().toString());
     }
 
-    public static void clearCookie(HttpServletResponse res, String path, String domain, String name, boolean http, boolean secure) {
+    public static void clearCookie(HttpServletResponse res,  String name, String domain, String path, boolean http, boolean secure) {
         ResponseCookie.ResponseCookieBuilder cookieBuilder = ResponseCookie.from(name, "")
                 .httpOnly(http)
                 .maxAge(0)
@@ -26,6 +31,14 @@ public class CookieUtil {
                 .path(path)
                 .domain(domain);
         res.addHeader(HttpHeaders.SET_COOKIE, cookieBuilder.build().toString());
+    }
+
+    public static String getDomain(HttpServletRequest req) {
+        String domain = req.getRequestURL().toString().replaceAll(req.getRequestURI(), "");
+        if(domain.contains("/"))
+            domain = domain.substring(domain.lastIndexOf('/') + 1, /* ':' uses in local servers. */ (domain.contains(":") ? domain.lastIndexOf(':') : domain.length()));
+
+        return domain;
     }
 
 }
