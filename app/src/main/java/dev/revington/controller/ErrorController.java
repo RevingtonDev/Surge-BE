3879f43c0128f67af154e3a2899b3011f7719ebd
RevingtonDev <revington.dev@gmail.com>
2022-07-02 22:48:56 +0530

    404

---
+++ app/src/main/java/dev/revington/controller/ErrorController.java
@@ -0,0 +1,16 @@
+package dev.revington.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "error";
+    }
+
+}
