4d909ac835a42908d90b8c41dc9eecb79abc6738
RevingtonDev <revington.dev@gmail.com>
2022-07-02 22:55:11 +0530

    Application constants. (Parameter names etc.)

---
+++ app/src/main/java/dev/revington/variables/Parameter.java
@@ -0,0 +1,13 @@
+package dev.revington.variables;
+
+public class Parameter {
+
+    // Client application API request parameter values
+    public static final String PASSWORD = "password";
+    public static final String EMAIL = "email";
+    public static final String ACCOUNT_TYPE = "accountType";
+
+    // Client application cookie names
+    public static final String COOKIE_TOKEN = "access_token";
+
+}
