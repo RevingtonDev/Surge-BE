bd5679dca06667e73eb797e559ad258f4c764a7f
RevingtonDev <revington.dev@gmail.com>
2022-07-03 22:17:17 +0530

    Major query changes.

--- app/src/main/java/dev/revington/repo/UserRepository.java
+++ app/src/main/java/dev/revington/repo/UserRepository.java
@@ -8,19 +8,25 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, Integer> {
 
     @Query(value =
         "{ $or : [" +
-                "{" +
-                    "$expr: {" +
-                        "$regexMatch: {" +
-                            "\"input\": {\"$toString\": \"$_id\"}," +
-                            "\"regex\": '?0'" +
+                "{ $and: [" +
+                    "{" +
+                        "$expr: {" +
+                            "$regexMatch: {" +
+                                "\"input\": {\"$toString\": \"$_id\"}, " +
+                                "\"regex\": '?0'" +
+                            "}" +
                         "}" +
+                    "}," +
+                    "{" +
+                        "_id: { $not: { $eq: ?1 } }" +
                     "}" +
-                "}," +
+                "]},"+
                 "{" +
                         "firstName: {$regex: /?0/i}" +
                 "}," +
@@ -28,23 +34,49 @@ public interface UserRepository extends MongoRepository<User, Integer> {
                         "lastName: {$regex: /?0/i}" +
                 "}," +
                 "{" +
-                    "email: {$regex: '?0'}" +
+                    "$and: [" +
+                        "{ email: { $regex: /?0/i } }, " +
+                        "{ email: { $not: { $eq: ?2 } } }" +
+                    "]" +
                 "}" +
             "]" +
-        "}", fields = "{'firstName': 1, 'lastName': 1, 'dateOfBirth': 1, 'email': 1, 'mobile': 1}")
-    public Page<User> find(String param, Pageable pageable);
+        "}", fields = "{password: 0}")
+    public Page<User> find(String param, int id, String email, Pageable pageable);
 
-    @Query(fields = "{'firstName': 1, 'lastName': 1, 'dateOfBirth': 1, 'email': 1, 'mobile': 1}")
+    @Query(fields = "{password: 0}")
     public Page<User> findById(int param, Pageable pageable);
-    @Query(fields = "{'firstName': 1, 'lastName': 1, 'dateOfBirth': 1, 'email': 1, 'mobile': 1}")
+
+    @Query(fields = "{password: 0}")
     public Page<User> findByFirstName(String param, Pageable pageable);
-    @Query(fields = "{'firstName': 1, 'lastName': 1, 'dateOfBirth': 1, 'email': 1, 'mobile': 1}")
+
+    @Query(fields = "{password: 0}")
     public Page<User> findByLastName(String param, Pageable pageable);
-    @Query(fields = "{'firstName': 1, 'lastName': 1, 'dateOfBirth': 1, 'email': 1, 'mobile': 1}")
+
+    @Query(fields = "{password: 0}")
     public Page<User> findByEmail(String param, Pageable pageable);
 
     public List<User> findByEmail(String param);
 
+    @Query(fields = "{ password: 0 }")
+    public Optional<User> findById(int param);
+
     @Query(value = "{}", sort = "{ '_id': -1 }")
     List<User> findAllByIdSortDescending();
+
+    @Query(value = "{ _id: { $not: { $eq : ?0 } } }", fields = "{ password: 0 }")
+    Page<User> findAll(int id, Pageable pageable);
+
+    @Query(value = "{ accountType: \"student\" }", fields = "{ password: 0, accountType: 0 }")
+    Page<User> findStudents(Pageable pageable);
+
+    @Query(value =
+            "{ $and: [ " +
+                "{ " +
+                    " _id: { $not: { $eq : ?0 } } " +
+                "}," +
+                "{ " +
+                    "accountType: \"admin\"" +
+                "} " +
+            "]}", fields = "{ password: 0, accountType: 0 }")
+    Page<User> findAdmins(int id, Pageable pageable);
 }
