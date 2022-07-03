da7554dbb90dcd28e4db91eb1da010e5edaf2013
RevingtonDev <revington.dev@gmail.com>
2022-07-03 22:26:30 +0530

    AccessToken token authization.

--- app/src/main/java/dev/revington/api/Admin.java
+++ app/src/main/java/dev/revington/api/Admin.java
@@ -7,9 +7,7 @@ import dev.revington.repo.UserRepository;
 import dev.revington.security.Crypto;
 import dev.revington.status.StatusHandler;
 import dev.revington.util.AccessToken;
-import dev.revington.util.Roles;
 import dev.revington.variables.Parameter;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import net.minidev.json.JSONObject;
@@ -20,16 +18,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.management.relation.Role;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.List;
-
-@RestController()
+@RestController
 @RequestMapping("/admin")
 public class Admin {
 
-    private final String ROLE = Roles.ADMIN.label;
+    private final String ROLE = Parameter.ADMIN;
 
     @Autowired
     TokenRepository accessRepo;
@@ -37,20 +30,20 @@ public class Admin {
     @Autowired
     UserRepository repo;
 
+
     @PutMapping("/create")
     public ResponseEntity<JSONObject> createAccount(HttpServletRequest req, HttpServletResponse res, @RequestBody JSONObject user) {
         User admin = AccessToken.validate(req, res, accessRepo, repo);
-        if(admin == null)
-            return new ResponseEntity<>(StatusHandler.E1002, HttpStatus.UNAUTHORIZED);
 
-        if(!admin.getAccountType().equals(ROLE))
-            return new ResponseEntity<>(StatusHandler.E1003, HttpStatus.UNAUTHORIZED);
+        ResponseEntity<JSONObject> response;
+        if((response = AccessToken.tokenAuthorization(admin, ROLE)) != null)
+            return response;
 
         if(!repo.findByEmail(user.getAsString(Parameter.EMAIL)).isEmpty())
             return new ResponseEntity<>(StatusHandler.E1004, HttpStatus.NOT_ACCEPTABLE);
 
         User rookie = new User(user.getAsString(Parameter.EMAIL), Crypto.getMD5(user.getAsString(Parameter.PASSWORD)));
-        rookie.set_id(Application.ID++);
+        rookie.setId(Application.ID++);
         rookie.setAccountType(user.getAsString(Parameter.ACCOUNT_TYPE));
         rookie.setTemporary(true);
         repo.save(rookie);
