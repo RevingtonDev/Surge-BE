94ed2977b69850a4547f93b6aec438711e4154b8
RevingtonDev <revington.dev@gmail.com>
2022-07-03 10:20:18 +0530

    Root user is added on application start & Global volatile variable to autoIncrement _id.

--- app/src/main/java/dev/revington/Application.java
+++ app/src/main/java/dev/revington/Application.java
@@ -1,15 +1,46 @@
 package dev.revington;
 
-import net.minidev.json.JSONObject;
+import dev.revington.entity.User;
+import dev.revington.repo.UserRepository;
+import dev.revington.security.Crypto;
+import dev.revington.util.Roles;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
+import java.util.Date;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    public static volatile int ID = 0;
+
+    @Autowired
+    UserRepository repo;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        ID = repo.findAllByIdSortDescending().get(0).get_id() + 1;
+
+        if(!repo.findByEmail("root@surge.com").isEmpty())
+            return;
+
+        User root = new User();
+        root.setId(ID++);
+        root.setFirstName("root");
+        root.setLastName("user");
+        root.setEmail("root@surge.com");
+        root.setPassword(Crypto.getMD5("root"));
+        root.setStatus(true);
+        root.setTemporary(false);
+        root.setAccountType(Roles.ADMIN.label);
+        repo.save(root);
     }
 }
