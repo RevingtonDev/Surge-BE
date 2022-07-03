e62b9c848d40ffd5e3b77e1e98c64bca8f721cb4
RevingtonDev <revington.dev@gmail.com>
2022-07-03 10:18:09 +0530

    Implemented an API for administrator to create new accounts.

--- app/src/main/java/dev/revington/api/Admin.java
+++ app/src/main/java/dev/revington/api/Admin.java
@@ -1,6 +1,75 @@
 package dev.revington.api;
 
+import dev.revington.Application;
+import dev.revington.entity.User;
+import dev.revington.repo.TokenRepository;
+import dev.revington.repo.UserRepository;
+import dev.revington.security.Crypto;
+import dev.revington.status.StatusHandler;
+import dev.revington.util.AccessToken;
+import dev.revington.util.Roles;
+import dev.revington.variables.Parameter;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import net.minidev.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.management.relation.Role;
+import java.util.Base64;
+import java.util.HashMap;
+
+@RestController()
+@RequestMapping("/admin")
 public class Admin {
+
+    private final String ROLE = Roles.ADMIN.label;
+
+    @Autowired
+    TokenRepository accessRepo;
+
+    @Autowired
+    UserRepository repo;
+
+    @PutMapping("/create")
+    public ResponseEntity<JSONObject> createAccount(HttpServletRequest req, HttpServletResponse res, @RequestBody JSONObject user) {
+        String token = "";
+        Cookie[] cookies = req.getCookies();
+
+        if(cookies.length <= 0)
+            return new ResponseEntity<>(StatusHandler.E1002, HttpStatus.UNAUTHORIZED);
+
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals(Parameter.COOKIE_TOKEN))
+                token = new String(Base64.getUrlDecoder().decode(cookie.getValue()));
+        }
+        cookies = null;
+
+        if (token.equals(""))
+            return new ResponseEntity<>(StatusHandler.E1002, HttpStatus.UNAUTHORIZED);
+
+        User admin = AccessToken.validate(req, res, accessRepo, repo, token);
+        if(admin == null)
+            return new ResponseEntity<>(StatusHandler.E1002, HttpStatus.UNAUTHORIZED);
+
+        if(!admin.getAccountType().equals(ROLE))
+            return new ResponseEntity<>(StatusHandler.E1003, HttpStatus.UNAUTHORIZED);
+
+        if(!repo.findByEmail(user.getAsString(Parameter.EMAIL)).isEmpty())
+            return new ResponseEntity<>(StatusHandler.E1004, HttpStatus.NOT_ACCEPTABLE);
+
+        User rookie = new User(user.getAsString(Parameter.EMAIL), Crypto.getMD5(user.getAsString(Parameter.PASSWORD)));
+        rookie.setId(Application.ID++);
+        rookie.setAccountType(user.getAsString(Parameter.ACCOUNT_TYPE));
+        rookie.setTemporary(true);
+        repo.save(rookie);
+        return new ResponseEntity<>(StatusHandler.S200, HttpStatus.OK);
+    }
 
 }
