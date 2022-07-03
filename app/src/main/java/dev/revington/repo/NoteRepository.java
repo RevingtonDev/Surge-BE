9eb8b89b5d51bf817b0958b0b85cc52c4bcbb7ce
RevingtonDev <revington.dev@gmail.com>
2022-07-03 22:19:05 +0530

    Query changes.

--- app/src/main/java/dev/revington/repo/NoteRepository.java
+++ app/src/main/java/dev/revington/repo/NoteRepository.java
@@ -1,14 +1,18 @@
 package dev.revington.repo;
 
 import dev.revington.entity.Note;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface NoteRepository extends MongoRepository<Note, Integer> {
+public interface NoteRepository extends MongoRepository<Note, String> {
 
-    @Query(value = "{'_id': '0'}")
-    public List<Note> findById(int id);
+    @Override
+    @Query(value = "{_id: ObjectId(?0)}")
+    Optional<Note> findById(String s);
 
 }
