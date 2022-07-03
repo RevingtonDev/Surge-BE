d6563d115b9c36cc77d35d8a60550a0e2862fbcb
RevingtonDev <revington.dev@gmail.com>
2022-07-03 22:17:54 +0530

    Replaced List<T> usage for singular data objects with Optional<T>.

--- app/src/main/java/dev/revington/repo/TokenRepository.java
+++ app/src/main/java/dev/revington/repo/TokenRepository.java
@@ -4,9 +4,10 @@ import dev.revington.entity.Token;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TokenRepository extends MongoRepository<Token, Integer> {
 
-    public List<Token> findByToken(String token);
+    public Optional<Token> findByToken(String token);
 
 }
