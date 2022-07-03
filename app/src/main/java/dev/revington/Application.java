412bea7b2868f3e133c77ee0844a62312db388ab
RevingtonDev <revington.dev@gmail.com>
2022-07-03 22:33:30 +0530

    Minor changes.

--- app/src/main/java/dev/revington/Application.java
+++ app/src/main/java/dev/revington/Application.java
@@ -3,15 +3,11 @@ package dev.revington;
 import dev.revington.entity.User;
 import dev.revington.repo.UserRepository;
 import dev.revington.security.Crypto;
-import dev.revington.util.Roles;
-import org.slf4j.LoggerFactory;
+import dev.revington.variables.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.Timestamp;
-import java.util.Date;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -27,7 +23,7 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        ID = repo.findAllByIdSortDescending().get(0).get_id() + 1;
+        ID = repo.findAllByIdSortDescending().get(0).getId() + 1;
 
         if(!repo.findByEmail("root@surge.com").isEmpty())
             return;
@@ -40,7 +36,7 @@ public class Application implements CommandLineRunner {
         root.setPassword(Crypto.getMD5("root"));
         root.setStatus(true);
         root.setTemporary(false);
-        root.setAccountType(Roles.ADMIN.label);
+        root.setAccountType(Parameter.ADMIN);
         repo.save(root);
     }
 }
